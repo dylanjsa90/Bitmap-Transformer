1735252006f8d05603120c3d2818092e122b4a62
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-15 10:14:31 -0700

    inital tests

--- test/testBitmap.js
+++ test/testBitmap.js
@@ -1,2 +1,13 @@
 const expect = require('chai').expect;
 const fs = require('fs');
+var paletteBitmapBuffer = fs.readFileSync(__dirname + '/palette-bitmap.bmp');
+var nonPaletteBitmapBuffer = fs.readFileSync(__dirname + '/non-palette-bitmap.bmp');
+
+
+describe('test the bitmap buffer', function() {
+  debugger;
+  it('should check the size of the buffers', function() {
+    expect(paletteBitmapBuffer.length).to.eql(11078);
+    expect(nonPaletteBitmapBuffer.length).to.eql(30054);
+  });
+});
