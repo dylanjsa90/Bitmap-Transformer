79cd6c6e2e3509315d4db98a3bf46e825445bcbb
dylanjsa90 <dylanjsanders1@gmail.com>
2016-07-18 10:11:16 -0700

    prepull commit

--- test/testBitmap.js
+++ test/testBitmap.js
@@ -2,12 +2,23 @@ const expect = require('chai').expect;
 const fs = require('fs');
 var paletteBitmapBuffer = fs.readFileSync(__dirname + '/palette-bitmap.bmp');
 var nonPaletteBitmapBuffer = fs.readFileSync(__dirname + '/non-palette-bitmap.bmp');
-
+var size = paletteBitmapBuffer.readUInt32LE(2);
+var bitmapWidth = paletteBitmapBuffer.readInt32LE(18);
+var bitmapHeight = paletteBitmapBuffer.readInt32LE(22);
+var bitsPerPixelPalette = paletteBitmapBuffer.readUInt16LE(28);
+var bitsPerPixelNonPalette = nonPaletteBitmapBuffer.readUInt16LE(28);
 
 describe('test the bitmap buffer', function() {
-  debugger;
   it('should check the size of the buffers', function() {
-    expect(paletteBitmapBuffer.length).to.eql(11078);
-    expect(nonPaletteBitmapBuffer.length).to.eql(30054);
+    expect(size).to.eql(11078);
+    expect(bitmapWidth).to.eql(100);
+    expect(bitmapHeight).to.eql(100);
+    expect(bitsPerPixelPalette).to.eql(8);
+    expect(bitsPerPixelNonPalette).to.eql(24);
+
   });
+
+  // it('should check for errors on transform name', () => {
+  //
+  // })
 });
